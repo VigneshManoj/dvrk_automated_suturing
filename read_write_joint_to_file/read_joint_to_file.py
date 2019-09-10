331c2317dacfbbb3bf40bf150ee946fa9d88bfc2
Vignesh <vvarier@wpi.edu>
2019-09-10 15:17:56 -0400

    tried making reading write data into a class called DataCollection

--- read_write_joint_to_file/read_joint_to_file.py
+++ read_write_joint_to_file/read_joint_to_file.py
@@ -4,31 +4,46 @@ import dvrk
 import numpy as np
 import rospy
 import math
-import csv
 
-# from read_write_joint_to_file import MoveDVRKArm
-file_heading_exits = 0
 
-if __name__ == '__main__':
-    rospy.init_node('move_davinci')
-    rate = rospy.Rate(500.0)
-    # Create a Python proxy for PSM1, name must match ros namespace
-    p = dvrk.psm('PSM1')
-    p.home()
-    # p.move_joint_one(0.05, 0)
-    data_file_dir = "/home/aimlabx/PycharmProjects/dvrk_automated_suturing/data/dvrk_joint_data.csv"  # where you want the file to be downloaded to
-    csv = open(data_file_dir, "a")
+class DataCollection:
+    # To make sure the headings exist in the csv file and is only written once
+    file_heading_exits = 0
 
-    current_pos = p.get_current_joint_position()
-    print("\nThe current joint position is ", current_pos, "\n")
-    if file_heading_exits == 0:
-        columnTitleRow = "Joint 1, Joint 2, Joint 3, Joint 4, Joint 5, Joint 6\n"
-        csv.write(columnTitleRow)
-        file_heading_exits = 1
-    for i in range(1, 6):
-        # row =  current_pos[0] + "," + current_pos[1] + "," + current_pos[2] + "," + current_pos[3] + "," + current_pos[4] + "," + current_pos[5]"\n"
-        row = str(current_pos[0])
-        row += "," + str(current_pos[i])
-        if i == 6:
-            row += "\n"
-        csv.write(row)
+    def __init__(self, loop_rate):
+        self.loop_rate = loop_rate
+
+    def read_joint_write_file(self):
+        rospy.init_node('read_write_data_dvrk')
+        rate = rospy.Rate(self.loop_rate)
+        # Create a Python proxy for PSM1, name must match ros namespace
+        p = dvrk.psm('PSM1')
+        p.home()
+        # p.move_joint_one(0.05, 0)
+        # Location of file storage
+        data_file_dir = "/home/aimlabx/PycharmProjects/dvrk_automated_suturing/data/dvrk_joint_data.csv"
+        csv = open(data_file_dir, "a")
+        # For writing the heading to the csv file
+        if self.file_heading_exits == 0:
+            column_title = "Joint 1, Joint 2, Joint 3, Joint 4, Joint 5, Joint 6\n"
+            csv.write(column_title)
+            self.file_heading_exits = 1
+        # While loop to read and write data continuously
+        while not rospy.is_shutdown():
+            # Read joint positions of dvrk arm
+            current_pos = p.get_current_joint_position()
+            print("\nThe current joint position is ", current_pos, "\n")
+            # Write the data to file
+            # For writing each joint position value
+            for i in range(1, 6):
+                # row =  current_pos[0] + "," + current_pos[1] + "," + current_pos[2] + "," + current_pos[3] + "," + current_pos[4] + "," + current_pos[5]"\n"
+                row = str(current_pos[0])
+                row += "," + str(current_pos[i])
+                # New set of data starts in a new line
+                if i == 6:
+                    row += "\n"
+                csv.write(row)
+            rate.sleep()
+
+
+
--- test_code.py
+++ test_code.py
@@ -1,75 +1,75 @@
+# # import dvrk
+# # import PyKDL
+# # from read_write_joint_to_file import MoveDVRKArm
+# #
+# # # Create a Python proxy for PSM1, name must match ros namespace
+# # # p = dvrk.psm('PSM1')
+# # # p.home()
+# # # p.dmove_joint_one(-0.05, 2) # move 3rd joint
+# # obj = MoveDVRKArm(0.05, 0, 0, 0, 0)
+# # print("reached here")
+# # #p.dmove(PyKDL.Vector(0.0, 0.02, 0.0)) # 5 cm in Y direction
+# # # p.move(PyKDL.Vector(0.0, 0.0, 0.0))
+# # obj.move_dvrk_arm_single_joint()
+#
+#
+# # !/usr/bin/env python
+# #
 # import dvrk
-# import PyKDL
-# from read_write_joint_to_file import MoveDVRKArm
-#
-# # Create a Python proxy for PSM1, name must match ros namespace
-# # p = dvrk.psm('PSM1')
-# # p.home()
-# # p.dmove_joint_one(-0.05, 2) # move 3rd joint
-# obj = MoveDVRKArm(0.05, 0, 0, 0, 0)
-# print("reached here")
-# #p.dmove(PyKDL.Vector(0.0, 0.02, 0.0)) # 5 cm in Y direction
-# # p.move(PyKDL.Vector(0.0, 0.0, 0.0))
-# obj.move_dvrk_arm_single_joint()
-
-
-# !/usr/bin/env python
-#
-import dvrk
-# import numpy
-# import rospy
-# import math
-# import PyKDL
-# i = 0.25
-# j, k = 0, 0
-if __name__ == '__main__':
-#     rospy.init_node('move_davinci1')
-#     # Create a Python proxy for PSM1, name must match ros namespace
-     p = dvrk.psm('PSM1')
-     p.home()
-#     # r = PyKDL.Rotation()
-#     rate = rospy.Rate(500.0)
-#     while not rospy.is_shutdown():
-#         # print "reaching here ", math.pi * i
-#         p.dmove_joint_one(i, 0)
-#
-#         if j <= 3:
-#             print "j val is ", j
-#             j = j + 1
-#             i = 0.25
-#             k = 0
-#         else:
-#             k = k + 1
-#             print "k val is ", k
-#             i = -0.25
-#             if k > 3:
-#                 j = 0
-#
-     current_pos = p.get_current_joint_position()
-     print("current pos is ", current_pos[0])
-#         current_vel = p.get_current_joint_velocity()
-#         current_effort = p.get_current_joint_effort()
-#         print "Current pos is ", current_pos
-#         print "Current vel is ", current_vel
-#         print "Current effort is ", current_effort
-#         rate.sleep()
-#
-# #example of writing data to a CSV
-#
-# dic = {"John": "john@example.com", "Mary": "mary@example.com"} #dictionary
-#
-# download_dir = "/home/aimlabx/PycharmProjects/dvrk_automated_suturing/data/dvrk_joint_data.csv" #where you want the file to be downloaded to
-#
-# csv = open(download_dir, "w")
-# #"w" indicates that you're writing strings to the file
-#
-# columnTitleRow = "name, email\n"
-# csv.write(columnTitleRow)
-#
-# for key in dic.keys():
-# 	name = key
-# 	email = dic[key]
-# 	row = name + "," + email + "\n"
-# 	csv.write(row)
-#
-#
+# # import numpy
+# # import rospy
+# # import math
+# # import PyKDL
+# # i = 0.25
+# # j, k = 0, 0
+# if __name__ == '__main__':
+# #     rospy.init_node('move_davinci1')
+# #     # Create a Python proxy for PSM1, name must match ros namespace
+#      p = dvrk.psm('PSM1')
+#      p.home()
+# #     # r = PyKDL.Rotation()
+# #     rate = rospy.Rate(500.0)
+# #     while not rospy.is_shutdown():
+# #         # print "reaching here ", math.pi * i
+# #         p.dmove_joint_one(i, 0)
+# #
+# #         if j <= 3:
+# #             print "j val is ", j
+# #             j = j + 1
+# #             i = 0.25
+# #             k = 0
+# #         else:
+# #             k = k + 1
+# #             print "k val is ", k
+# #             i = -0.25
+# #             if k > 3:
+# #                 j = 0
+# #
+#      current_pos = p.get_current_joint_position()
+#      print("current pos is ", current_pos[0])
+# #         current_vel = p.get_current_joint_velocity()
+# #         current_effort = p.get_current_joint_effort()
+# #         print "Current pos is ", current_pos
+# #         print "Current vel is ", current_vel
+# #         print "Current effort is ", current_effort
+# #         rate.sleep()
+# #
+# # #example of writing data to a CSV
+# #
+# # dic = {"John": "john@example.com", "Mary": "mary@example.com"} #dictionary
+# #
+# # download_dir = "/home/aimlabx/PycharmProjects/dvrk_automated_suturing/data/dvrk_joint_data.csv" #where you want the file to be downloaded to
+# #
+# # csv = open(download_dir, "w")
+# # #"w" indicates that you're writing strings to the file
+# #
+# # columnTitleRow = "name, email\n"
+# # csv.write(columnTitleRow)
+# #
+# # for key in dic.keys():
+# # 	name = key
+# # 	email = dic[key]
+# # 	row = name + "," + email + "\n"
+# # 	csv.write(row)
+# #
+# #
