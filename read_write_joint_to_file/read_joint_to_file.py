61f00cba6050df7700912094109897d5aceb0740
Vignesh <vvarier@wpi.edu>
2019-09-10 15:49:17 -0400

    added writing of the joint velocity data to csv file

--- read_write_joint_to_file/read_joint_to_file.py
+++ read_write_joint_to_file/read_joint_to_file.py
@@ -25,24 +25,34 @@ class DataCollection:
         csv = open(data_file_dir, "a")
         # For writing the heading to the csv file
         if self.file_heading_exits == 0:
-            column_title = "Joint 1, Joint 2, Joint 3, Joint 4, Joint 5, Joint 6\n"
+            column_title = "Pos Joint 1, Pos Joint 2, Pos Joint 3, Pos Joint 4, Pos Joint 5, Pos Joint 6, " \
+                           "Vel Joint 1, Vel Joint 2, Vel Joint 3, Vel Joint 4, Vel Joint 5, Vel Joint 6 \n"
             csv.write(column_title)
             self.file_heading_exits = 1
         # While loop to read and write data continuously
         while not rospy.is_shutdown():
             # Read joint positions of dvrk arm
             current_pos = p.get_current_joint_position()
-            print("\nThe current joint position is ", current_pos, "\n")
+            # Read joint velocities of dvrk arm
+            current_vel = p.get_current_joint_velocity()
+            # print("\nThe current joint position is ", current_pos, "\n")
+            # print("\nThe current joint position is ", current_vel, "\n")
+            
+            # Initialize the row data with joint 0 position
             row_data = str(current_pos[0])
             # Write the data to file
             # For writing each joint position value
             for i in range(1, 6):
                 row_data += "," + str(current_pos[i])
-                print("\n I value is ", i, "\n")
-                # New set of data starts in a new line
+                # print("\n I value is ", i, "\n")
                 if i == 5:
-                    row_data = row_data + "\n"
-                    print("reached inside the next line")
+                    # For writing each joint velocity value
+                    row_data += "," + str(current_vel[0])
+                    for j in range(1, 6):
+                        row_data += "," + str(current_vel[j])
+                        # New set of data starts in a new line
+                        if j == 5:
+                            row_data = row_data + "\n"
             # Finally write the data in row_data to the csv file
             csv.write(row_data)
             # Decides at what rate the data is written to the csv file
