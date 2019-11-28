931cdba48745dee9928eac2612b412f14a9d8a47
Vignesh <vigneshvarier@gmail.com>
2019-11-27 19:30:00 -0500

    reward,feature,statespace into 1 Class.works without error till here

--- max_ent_trial3/create_state_space.py
+++ max_ent_trial3/create_state_space.py
@@ -3,7 +3,7 @@ import numba as nb
 import math
 
 
-class RobotStateSpace:
+class RobotMarkovModel:
     def __init__(self):
         # Model here means the 3D cube being created
         # linspace limit values: limit_values_angle = [[-0.5, 0.5], [-0.234, -0.155], [0.28, 0.443]]
@@ -17,6 +17,8 @@ class RobotStateSpace:
         self.model_limits_pos_x_val = np.linspace(-0.009, -0.003, 11, dtype='float16')
         self.model_limits_pos_y_val = np.linspace(-0.009, -0.003, 11, dtype='float16')
         self.model_limits_pos_z_val = np.linspace(-0.009, -0.003, 11, dtype='float16')
+        # There are 6 parameters defining a state value of the robot, RPY and XYZ
+        self.n_states = 6
         # The created model state values
         self.model_rot_par_r = []
         self.model_rot_par_p = []
@@ -24,6 +26,11 @@ class RobotStateSpace:
         self.model_end_pos_x = []
         self.model_end_pos_y = []
         self.model_end_pos_z = []
+        trajectories = np.genfromtxt\
+            ("/home/vignesh/PycharmProjects/dvrk_automated_suturing/data/sample_trajectory_data_without_norm.csv",
+             delimiter=",")
+        self.state_trajectories = trajectories[:, 0:6]
+        self.action_trajectories = trajectories[:, 6:12]
 
     def state_space_model(self):
 
@@ -41,9 +48,56 @@ class RobotStateSpace:
         return self.model_rot_par_r, self.model_rot_par_p, self.model_rot_par_y, \
                self.model_end_pos_x, self.model_end_pos_y, self.model_end_pos_z
 
-    def trajectories_data(self, file_path):
-        trajectories = np.genfromtxt(file_path, delimiter=",")
-        state_trajectories = trajectories[:, 0:6]
-        action_trajectories = trajectories[:, 6:12]
+    def trajectories_data(self):
+        # Return trajectories data if any function requires it outside this class
+        return self.state_trajectories, self.action_trajectories
 
-        return state_trajectories, action_trajectories
+    def trajectories_features_array(self, weights):
+        # Creates the array of features and rewards for the whole trajectory
+        # It calls the RobotMarkovModel class reward function which returns the reward and features for that specific
+        # state values. These values are repeatedly added until the length of trajectory
+        trajectories_reward = []
+        trajectories_features = []
+        trajectory_reward = np.zeros([1, 1], dtype='float32')
+        trajectory_features = np.zeros([2, 1], dtype='float32')
+        for i in range(0, self.state_trajectories.shape[0]):
+            # Reads only the state trajectory data
+            rot_par_r = self.state_trajectories[i, 0]
+            rot_par_p = self.state_trajectories[i, 1]
+            rot_par_y = self.state_trajectories[i, 2]
+            end_pos_x = self.state_trajectories[i, 3]
+            end_pos_y = self.state_trajectories[i, 4]
+            end_pos_z = self.state_trajectories[i, 5]
+
+            rewards, features = self.reward_func(rot_par_r, rot_par_p, rot_par_y,
+                                                             end_pos_x, end_pos_y, end_pos_z, weights)
+            trajectory_reward = trajectory_reward + rewards
+            trajectory_features = trajectory_features + np.vstack((features[0], features[1]))
+        trajectories_reward.append(trajectory_reward)
+        trajectories_features.append(trajectory_features)
+        # Returns the array of trajectory features and reward
+        return trajectories_features, trajectories_reward
+
+    # Calculates reward function
+    def reward_func(self, rot_par_r, rot_par_p, rot_par_y, end_pos_x, end_pos_y, end_pos_z, weights):
+
+        features = [self.features_array_func, self.features_array_sec_func]
+        reward = 0
+        features_arr = []
+        for n in range(0, len(features)):
+            features_arr.append(features[n](rot_par_r, rot_par_p, rot_par_y, end_pos_x, end_pos_y, end_pos_z))
+            reward = reward + weights[0, n]*features_arr[n]
+        # Created the feature function assuming everything has importance, so therefore added each parameter value
+        return reward, features_arr
+
+    # Created feature set1 which basically takes the exponential of sum of individually squared value
+    def features_array_func(self, rot_par_r, rot_par_p, rot_par_y, end_pos_x, end_pos_y, end_pos_z):
+        feature_1 = np.exp(-(rot_par_r**2 + rot_par_p**2 + rot_par_y**2 + end_pos_x**2 + end_pos_y**2 + end_pos_z**2))
+        return feature_1
+
+    # Created feature set2 which basically takes the exponential of sum of individually squared value divided by
+    # the variance value
+    def features_array_sec_func(self, rot_par_r, rot_par_p, rot_par_y, end_pos_x, end_pos_y, end_pos_z):
+        feature_2 = np.exp(-(rot_par_r**2 + rot_par_p**2 + rot_par_y**2 + end_pos_x**2 + end_pos_y**2 + end_pos_z**2)/0.1**2)
+        # print f2
+        return feature_2
--- max_ent_trial3/main.py
+++ max_ent_trial3/main.py
@@ -1,25 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from create_state_space import RobotStateSpace
-from robot_mdp import RobotMarkovModel
+from create_state_space import RobotMarkovModel
 
 
-def main(discount, n_trajectories, epochs, learning_rate, file_path):
-    robot_space = RobotStateSpace()
-    robot_mdp = RobotMarkovModel()
+def main(discount, n_trajectories, epochs, learning_rate, weights):
+    robot_space = RobotMarkovModel()
+    # robot_mdp = RobotMarkovModel()
     model_rot_par_r, model_rot_par_p, model_rot_par_y, \
     model_end_pos_x, model_end_pos_y, model_end_pos_z = robot_space.state_space_model()
 
-    state_trajectories, action_trajectories = robot_space.trajectories_data(file_path)
-    features_array = robot_mdp.features_array(model_rot_par_r, model_rot_par_p, model_rot_par_y,
-                                              model_end_pos_x, model_end_pos_y, model_end_pos_z)
-    # print "feature matrix is ", feature_matrix
-    ground_r = np.array([gw.reward(s) for s in range(gw.n_states)])
-    r = maxent.irl(feature_matrix, gw.n_actions, discount,
-                   gw.transition_probability, trajectories, epochs, learning_rate)
+    # state_trajectories, action_trajectories = robot_space.trajectories_data()
+    traj_features_array, traj_rewards_array = robot_space.trajectories_features_array(weights)
+    print traj_features_array
+    print traj_rewards_array
+    print model_rot_par_r
+    # reward_function = maxent.irl(feature_matrix, gw.n_actions, discount,
+    #               gw.transition_probability, trajectories, epochs, learning_rate)
 
 
 if __name__ == '__main__':
-    # Provide the location of the trajectory data to be used
-    trajectory_file_path = "/home/vignesh/PycharmProjects/dvrk_automated_suturing/data/sample_trajectory_data_without_norm.csv"
-    main(0.01, 20, 200, 0.01, trajectory_file_path)
+    rand_weights = np.random.rand(1, 2)
+    main(0.01, 20, 200, 0.01, rand_weights)
