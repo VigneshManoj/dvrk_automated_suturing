6990becb87d9c96da9e7a918f3cbb8d2ec90e716
Vignesh <vigneshvarier@gmail.com>
2019-12-10 16:04:26 -0500

    created transition probability function

--- validation_max_ent_pos/policy_iteration.py
+++ validation_max_ent_pos/policy_iteration.py
@@ -21,9 +21,6 @@ class RobotStateUtils(concurrent.futures.ThreadPoolExecutor):
         self.states = {}
         # Creates a dictionary for storing the action values
         self.action_space = {}
-        # Used for temporary assignment of action and state values while
-        self.action_set = []
-        self.state_set = []
         # Numerical values assigned to each action in the dictionary
         self.possible_actions = [i for i in range(27)]
         # Total Number of states defining the state of the robot
@@ -37,25 +34,30 @@ class RobotStateUtils(concurrent.futures.ThreadPoolExecutor):
     def create_state_space_model_func(self):
         # Creates the state space of the robot based on the values initialized for linspace by the user
         # print "Creating State space "
+        state_set = []
         for i_val in self.lin_space_limits:
             for j_val in self.lin_space_limits:
                 for k_val in self.lin_space_limits:
                     # Rounding state values so that the values of the model, dont take in too many floating points
-                    self.state_set.append([round(i_val, 1), round(j_val, 1), round(k_val, 1)])
+                    state_set.append([round(i_val, 1), round(j_val, 1), round(k_val, 1)])
         # Assigning the dictionary keys
-        for i in range(len(self.state_set)):
-            self.states[i] = self.state_set[i]
+        for i in range(len(state_set)):
+            dict = {i: state_set[i]}
+            self.states.update(dict)
+
         return self.states
 
     def create_action_set_func(self):
         # Creates the action space required for the robot. It is defined by the user beforehand itself
+        action_set = []
         for pos_x in [-0.1, 0, 0.1]:
             for pos_y in [-0.1, 0, 0.1]:
                 for pos_z in [-0.1, 0, 0.1]:
-                    self.action_set.append([pos_x, pos_y, pos_z])
+                    action_set.append([pos_x, pos_y, pos_z])
         # Assigning the dictionary keys
-        for i in range(len(self.action_set)):
-            self.action_space[i] = self.action_set[i]
+        for i in range(len(action_set)):
+            dict = {i: action_set[i]}
+            self.action_space.update(dict)
 
         return self.action_space
 
@@ -91,37 +93,93 @@ class RobotStateUtils(concurrent.futures.ThreadPoolExecutor):
 
         return resulting_state
 
+    def neighbouring(self, i, k):
+
+        return abs(i[0] - k[0]) + abs(i[1] - k[1]) + abs(i[2] - k[2]) <= 1
+
+    def _transition_probability(self, i, j, k):
+
+        si, sj, sk = self.states[i]
+        ai, aj, ak = self.action_space[j]
+        s_ni, s_nj, s_nk = self.states[k]
+
+        if not self.neighbouring((si, sj, sk), (s_ni, s_nj, s_nk)):
+            return 0.0
+
+        # Is k the intended state to move to?
+        if (si + ai, sj + aj, sk + ak) == (s_ni, s_nj, s_nk):
+            return 1
+
+        # If these are the same point, we can only move here by either moving
+        # off the grid or being blown off the grid. Are we on a corner or not?
+        if (si, sj, sk) in {(0, 0, 0), (self.grid_size-1, self.grid_size-1, self.grid_size-1),
+                            (0, self.grid_size-1, 0), (self.grid_size-1, 0, 0), (0, 0, self.grid_size-1)}:
+            # Corner.
+            # Can move off the edge in two directions.
+            # Did we intend to move off the grid?
+            if not (0 <= si + ai < self.grid_size and
+                    0 <= sj + aj < self.grid_size and
+                    0 <= sk + ak < self.grid_size):
+
+                return 1
+        else:
+            # Not a corner. Is it an edge?
+            if (si not in {0, self.grid_size-1} and
+                sj not in {0, self.grid_size-1} and
+                sk not in {0, self.grid_size-1}):
+                # Not an edge.
+                return 0.0
+
+            # Edge.
+            # Can only move off the edge in one direction.
+            # Did we intend to move off the grid?
+            if not (0 <= si + ai < self.grid_size and
+                    0 <= sj + aj < self.grid_size and
+                    0 <= sk + ak < self.grid_size):
+                # We intended to move off the grid, so we have the regular
+                # success chance of staying here.
+                return 1
+            else:
+                # We can blow off the grid only by wind.
+                return 0
+
     def calc_value_func(self, reward, discount, threshold):
-        v = np.zeros(len(self.states))
-        '''
+        n_states = len(self.states)
+        n_actions = len(self.action_space)
+        v = np.zeros(n_states)
+
         diff = float("inf")
         while diff > threshold:
             diff = 0
             for s in range(n_states):
+                print "states is", s
                 max_v = float("-inf")
                 for a in range(n_actions):
-                    max_v = max(max_v, reward + discount * v)
+                    tp = np.dstack([a]*self.grid_size)
+                    # print "action is ", a
+                     #print "max v is ", max_v
+                    # print "reward + vdiscount ", tp.flatten().shape, reward.shape, v.shape, discount
+                    max_v = max(max_v, np.dot(tp, reward + discount * v))
 
                 new_diff = abs(v[s] - max_v)
                 if new_diff > diff:
                     diff = new_diff
                 v[s] = max_v
-        '''
-        return v
 
+        return v
+    '''
     def find_policy(self, n_states, n_actions, reward, discount,
                     threshold=1e-2, v=None):
 
         if v is None:
-            v = self.calc_value_func(n_states, n_actions, reward,
-                              discount, threshold)
+            v = self.calc_value_func(reward, discount, threshold)
         def policy(s):
             return max(range(n_actions),
                        key=lambda a: sum((reward[k] + discount * v[k]) for k in range(n_states)))
 
         policy = np.array([policy(s) for s in range(n_states)])
         return policy
-
+    '''
     '''
     def generate_trajectories(self, n_trajectories, trajectory_length, policy, random_start=True):
 
@@ -173,14 +231,13 @@ if __name__ == '__main__':
     # Robot Object called
     # Pass the gridsize required
     obj_state_util = RobotStateUtils(11)
-    ele = obj_state_util.create_state_space_model_func()
-    # print ele
     states = obj_state_util.create_state_space_model_func()
     print len(states)
     action = obj_state_util.create_action_set_func()
     row_column = obj_state_util.get_state_val_index([-0.1, 0.1, 1.0])
     # print row_column
-    # print states[1220]
+    # print sorted(states.keys())
+    # print sorted(states.values())
     state_check = 32
     action_val = 1
     # print "Current state index ", state_check
@@ -194,8 +251,10 @@ if __name__ == '__main__':
     reward, features, n_features = obj_mdp.reward_func(r[0], r[1], r[2], weights)
     # print reward
     # print features
-    valuefunc = obj_state_util.calc_value_func(1, 2, 3)
-    print len(valuefunc)
+
+    reward_trial = np.ones(len(states))
+    valuefunc = obj_state_util.calc_value_func(reward_trial, 0.01, 1e-2)
+    print "value function is ", valuefunc
 
 
 
