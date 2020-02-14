8410a88d8335d541443edc790c39188076571bd2
Vignesh <vigneshvarier@gmail.com>
2020-02-14 15:06:34 -0500

    made changes to env & main file.still checking the validity of result
    solved problem of output not rendering repeatedly

--- max_ent_trial5_with_ddpg_stable_baseline/rl_environment_trial3.py
+++ max_ent_trial5_with_ddpg_stable_baseline/rl_environment_trial3.py
@@ -18,59 +18,54 @@ def goal_distance(goal_a, goal_b):
 
 class RobotEnv(gym.Env):
     metadata = {
-        'render.modes': ['human', 'rgb_array'],
+        'render.modes': ['human', 'rgb_array', 'ansi'],
         'video.frames_per_second': 50
     }
 
     def __init__(self):
-        self.limit_values = np.array([1, 1])
+        self.limit_values = 1
         self.action_space_dim = 2
         self.dt = 0.1  # seconds between state updates
-        self.t = 0
-        self.t_limit = 540
-        self.target = np.array([5.0, 5.0]).reshape((1, 2))
+
+        self.target = np.array([5.0, 5.0])
         self.threshold = 0.01
-        high = np.array([
-            np.finfo(np.float32).max,
-            np.finfo(np.float32).max])
+        # high = np.array([
+        #     np.finfo(np.float32).max,
+        #     np.finfo(np.float32).max])
 
-        self.action_space = spaces.Box(-self.limit_values, self.limit_values)
-        self.observation_space = spaces.Box(-high, high)
-
+        self.action_space = spaces.Box(-1., 1., shape=(self.action_space_dim,), dtype='float32')
+        self.observation_space = spaces.Box(-np.inf, np.inf, shape=(self.action_space_dim,), dtype='float32')
+        # self.action_space = spaces.Box(
+        #     low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
         self._seed()
         self.viewer = None
-        self.state = None
+        self.state = np.array([0, 0])
 
     def _seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
     def _step(self, action):
+        # print("action space is ", self.action_space)
         # Valid action
-        action = np.clip(action, -self.limit_values, self.limit_values)
+        action = np.clip(action, -1, +1).astype(np.float32)
 
-        state = self.state
-        x_val, y_val = state
-
-        x_update, y_update = np.array([x_val, y_val]) + action
-        # print("current state and action taken ", x_val, y_val, action, " result ", x_update, y_update)
-        self.state = (x_update, y_update)
-
+        new_state = self.state + action
+        print("current state and action taken ", self.state, action, " result and result shape ", new_state)
+        self.state = new_state
         goal = self.target
         done = False
         # print("x update and goal is ", x_update, goal[0][1], self.threshold)
-        if x_update - goal[0][0] < self.threshold and y_update - goal[0][1] < self.threshold:
+        if new_state[0] - goal[0] < self.threshold and new_state[1] - goal[0] < self.threshold:
             done = True
 
-        self.t += 1
-
-        if self.t >= self.t_limit:
+        # print("shape goal state action is ", goal.shape, np.array([x_val, y_val]).shape, action.shape)
+        reward = self.compute_reward(self.state, goal)
+        if abs(new_state[0]) >= 500.0 or abs(new_state[1]) >= 500.0:
             done = True
-        # print("goal shape is ", goal.shape)
-        reward = self.compute_reward(np.array([self.state]), goal)
-        obs = np.array([x_update, y_update])
-        x_val, y_val = x_update, y_update
-        return obs, reward, done, {}
+            reward -= 100
+        print("reward received ", reward)
+        return new_state, reward, done, {}
 
     def compute_reward(self, achieved_goal, goal):
         # Compute distance between goal and the achieved goal.
@@ -79,14 +74,22 @@ class RobotEnv(gym.Env):
 
     def _reset(self):
         # self.state = self.np_random.normal(loc=np.array([0.0, 0.0, 30*(2*np.pi)/360, 0.0]), scale=np.array([0.0, 0.0, 0.0, 0.0]))
-        self.state = np.random.normal(loc=np.array([0.0, 0.0]),
-                                           scale=np.array([0.02, 0.02]))
-        self.steps_beyond_done = None
-        self.t = 0  # timestep
-        x_val, y_val = self.state
-        obs = np.array([x_val, y_val])
+        random_state = np.random.normal(loc=np.array([0.0, 0.0]), scale=np.array([0.2, 0.2]))
+        obs = random_state
+        # print("random state, random state shape, observation shape is ", random_state, random_state.shape, obs.shape)
+
         return obs
 
-    def _render(self, mode='human', close=False):
+    def _render(self, mode='ansi', close=False):
         pass
 
+
+# if __name__ == '__main__':
+#     env = RobotEnv()
+#     action = np.array([0.1, 0.1])
+#     while True:
+#         env.render()
+#         obs, rewards, done, info = env.step(action)
+#         # if i % 100 == 0:
+#         # print("current state, action is ", _states, action, " resulting state ", obs)
+#         env.render(mode='ansi')
--- max_ent_trial5_with_ddpg_stable_baseline/trial_ddpg.py
+++ max_ent_trial5_with_ddpg_stable_baseline/trial_ddpg.py
@@ -9,23 +9,40 @@ import gym_robot
 env = gym.make('robot-v0')
 i = 0
 # the noise objects for DDPG
+# print("actions space ", np.abs(env.action_space.low), np.abs(env.action_space.high))
 n_actions = env.action_space.shape[-1]
 param_noise = None
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
 
-model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise)
+model = DDPG(MlpPolicy, env, verbose=1, param_noise=param_noise, action_noise=action_noise, render_eval=False)
 model.learn(total_timesteps=400000)
-model.save("ddpg_mountain")
+model.save("/home/vignesh/PycharmProjects/dvrk_automated_suturing/max_ent_trial5_with_ddpg_stable_baseline/ddpg_robot_1")
 
 del model # remove to demonstrate saving and loading
 
-model = DDPG.load("ddpg_mountain")
+model = DDPG.load("/home/vignesh/PycharmProjects/dvrk_automated_suturing/max_ent_trial5_with_ddpg_stable_baseline/ddpg_mountain")
 
-obs = env.reset()
-while True:
-    action, _states = model.predict(obs)
-    obs, rewards, done, info = env.step(action)
-    i += 1
-    if i % 100 == 0:
-        print("current state, action is ", _states, action, " resulting state ", obs)
-    env.render()
+print(env.action_space)
+# while True:
+#     obs = env.reset()
+#
+#     action, _states = model.predict(obs)
+#     obs, rewards, done, info = env.step(action)
+#     i += 1
+#     if i % 1000 == 0:
+#         print("current state, action is ", _states, action, " resulting state ", obs)
+#     env.render()
+
+for i in range(100):  # Total episodes to train
+    obs = env.reset()
+    print("observation ", obs)
+    done = False
+    while not done:
+        action, _states = model.predict(obs)
+
+        obs, rewards, done, info = env.step(action)
+        # print("current state, action is ", _states, action, " resulting state ", obs)
+        i += 1
+        if i % 1000 == 0:
+            print("current state, action is ", _states, action, " resulting state ", obs)
+        # env.render('ansi')
